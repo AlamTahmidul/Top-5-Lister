eeec6429f9f9c47cd5b4e01242d7b63f46b59082
Tahmidul Alam <31861624+AlamTahmidul@users.noreply.github.com>
2021-09-16 14:12:28 -0400

    statusbar change

---
+++ HW1/public_html/src/top5/Top5Controller.js
@@ -0,0 +1,114 @@
+/**
+ * Top5ListController.js
+ * 
+ * This file provides responses for all user interface interactions.
+ * 
+ * @author McKilla Gorilla
+ * @author ?
+ */
+export default class Top5Controller {
+    constructor() {
+
+    }
+
+    setModel(initModel) {
+        this.model = initModel;
+        this.initHandlers();
+    }
+
+    initHandlers() {
+        // SETUP THE TOOLBAR BUTTON HANDLERS
+        document.getElementById("add-list-button").onmousedown = (event) => {
+            let newList = this.model.addNewList("Untitled", ["?","?","?","?","?"]);            
+            this.model.loadList(newList.id);
+            this.model.saveLists();
+        }
+        document.getElementById("undo-button").onmousedown = (event) => {
+            this.model.undo();
+        }
+
+        // SETUP THE ITEM HANDLERS
+        for (let i = 1; i <= 5; i++) {
+            let item = document.getElementById("item-" + i);
+
+            // AND FOR TEXT EDITING
+            item.ondblclick = (ev) => {
+                if (this.model.hasCurrentList()) {
+                    // CLEAR THE TEXT
+                    item.innerHTML = "";
+
+                    // ADD A TEXT FIELD
+                    let textInput = document.createElement("input");
+                    textInput.setAttribute("type", "text");
+                    textInput.setAttribute("id", "item-text-input-" + i);
+                    textInput.setAttribute("value", this.model.currentList.getItemAt(i-1));
+
+                    item.appendChild(textInput);
+
+                    textInput.ondblclick = (event) => {
+                        this.ignoreParentClick(event);
+                    }
+                    textInput.onkeydown = (event) => {
+                        if (event.key === 'Enter') {
+                            this.model.addChangeItemTransaction(i-1, event.target.value);
+                        }
+                    }
+                    textInput.onblur = (event) => {
+                        this.model.addChangeItemTransaction(i-1, event.target.value);
+                        this.model.restoreList();
+                    }
+                }
+            }
+        }
+    }
+
+    registerListSelectHandlers(id) {
+        let list = document.getElementById("top5-list-" + id);
+        // FOR SELECTING THE LIST
+        list.onmousedown = (event) => {
+            this.model.unselectAll();
+
+            // GET THE SELECTED LIST
+            this.model.loadList(id);
+
+            // ALSO, UPDATE THE BOTTOM STATUS BAR
+            let cardText = document.getElementById("list-card-text-" + id);
+            let statusBar = document.getElementById("top5-statusbar");
+            statusBar.innerHTML = "Top 5 " + cardText.innerHTML;
+
+            // EDIT THE NAME OF THE LIST
+            list.ondblclick = (ev) => {
+                // TODO
+            }
+
+        }
+        // FOR DELETING THE LIST
+        document.getElementById("delete-list-" + id).onmousedown = (event) => {
+            this.ignoreParentClick(event);
+            // VERIFY THAT THE USER REALLY WANTS TO DELETE THE LIST
+            let modal = document.getElementById("delete-modal");
+            this.listToDeleteIndex = id;
+            let listName = this.model.getList(id).getName();
+            let deleteSpan = document.getElementById("delete-list-span");
+            deleteSpan.innerHTML = "";
+            deleteSpan.appendChild(document.createTextNode(listName));
+            modal.classList.add("is-visible");
+        }
+        // FOR ON MOUSE HOVER
+        list.onmouseover = (event) => {
+            // TODO
+            console.log("IT's HOVERED!!!");
+
+            // ON MOUSE OUT OF FOCUS
+            list.onmouseout = (event) => {
+                // TODO
+                console.log("IT's NOT HOVERED!!!");
+            }
+        }
+    }
+
+    ignoreParentClick(event) {
+        event.cancelBubble = true;
+        if (event.stopPropagation) event.stopPropagation();
+    }
+}
